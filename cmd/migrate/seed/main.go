3124929a70ff65e997113dced351e6864f2a7ead
CP Payne <charlethical1@gmail.com>
2024-10-21 10:07:30 +0200

    Implement seeding

---
+++ cmd/migrate/seed/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/CP-Payne/social/internal/db"
+	"github.com/CP-Payne/social/internal/env"
+	"github.com/CP-Payne/social/internal/store"
+)
+
+func main() {
+
+	addr := env.GetString("DB_ADDR", "postgres://postgres:postgres@localhost:5432/socialnetwork?sslmode=disable")
+
+	conn, err := db.New(addr, 15, 15, "15m")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	store := store.NewStorage(conn)
+
+	db.Seed(store)
+}

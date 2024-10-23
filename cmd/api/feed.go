e16a430be582ee793524024db8102f814f3520d6
CP Payne <charlethical1@gmail.com>
2024-10-23 08:41:04 +0200

    Implement feed search by tags, title and content

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -11,7 +11,7 @@ func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Reques
 	fq := store.PaginatedFeedQuery{
 		Limit:  20,
 		Offset: 0,
-		Sort:   "",
+		Sort:   "desc",
 	}
 
 	fq, err := fq.Parse(r)

a9aca6367bda05ccd8f3d501aadfc0c2f1b47b81
CP Payne <charlethical1@gmail.com>
2024-10-24 18:05:46 +0200

    Implement zap logging

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"log"
-
 	"github.com/CP-Payne/social/internal/db"
 	"github.com/CP-Payne/social/internal/env"
 	"github.com/CP-Payne/social/internal/store"
+	"go.uber.org/zap"
 )
 
 const version = "0.0.1"
@@ -41,22 +40,29 @@ func main() {
 		env: env.GetString("ENV", "development"),
 	}
 
+	// Logger
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	defer logger.Sync()
+
+	// Database
+
 	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
 	if err != nil {
-		log.Panic(err)
+		logger.Fatal(err)
 	}
 	defer db.Close()
-	log.Println("database connection pool established")
+	logger.Info("database connection pool established")
 
 	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
 		store:  store,
+		logger: logger,
 	}
 
 	mux := app.mount()
 
-	log.Fatal(app.run(mux))
+	logger.Fatal(app.run(mux))
 
 }

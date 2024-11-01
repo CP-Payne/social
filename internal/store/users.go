4578b686af8a2fbd1fd12815d548985d2eefa3b5
CP Payne <charlethical1@gmail.com>
2024-11-01 13:42:11 +0200

    Fixing user invitation

--- internal/store/users.go
+++ internal/store/users.go
@@ -90,12 +90,17 @@ func (s *UserStore) GetByID(ctx context.Context, userID int64) (*User, error) {
 func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
 	query := `
 		INSERT INTO users (username, password, email, role_id)
-		VALUES($1, $2, $3, $4)
+		VALUES($1, $2, $3, (SELECT id FROM roles WHERE name = $4))
 		RETURNING id, created_at	
 	`
 
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
+
+	role := user.Role.Name
+	if role == "" {
+		role = "user"
+	}
 
 	err := tx.QueryRowContext(
 		ctx,
@@ -103,7 +108,7 @@ func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
 		user.Username,
 		user.Password.hash,
 		user.Email,
-		user.RoleID,
+		role,
 	).Scan(
 		&user.ID,
 		&user.CreatedAt,

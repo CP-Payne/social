8153fb546beea95f50628e410c8e6ba47cd2333f
CP Payne <charlethical1@gmail.com>
2024-11-03 19:31:32 +0200

    Updated cors policy

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/CP-Payne/social/docs" // Required for swagger docs
 	"github.com/CP-Payne/social/internal/auth"
+	"github.com/CP-Payne/social/internal/env"
 	"github.com/CP-Payne/social/internal/mailer"
 	"github.com/CP-Payne/social/internal/ratelimiter"
 	"github.com/CP-Payne/social/internal/store"
@@ -94,20 +95,18 @@ type dbConfig struct {
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
 
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedOrigins:   []string{env.GetString("CORS_ALLOWED_ORIGIN", "http://localhost:5174")},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
 	if app.config.rateLimiter.Enabled {
 		r.Use(app.RateLimiterMiddleware)
 	}
